578eb1e30f9d2af6d1513d8f982d2e8198dca95d
Sergey UP <sergey10goals@gmail.com>
2024-03-16 13:11:14 +0200

    added gsap.from animation

--- src/pages/GsapFrom.jsx
+++ src/pages/GsapFrom.jsx
@@ -1,5 +1,18 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const GsapFrom = () => {
   // TODO: Implement the gsap.from() method
+  useGSAP(() => {
+gsap.from("#green-box", {
+  x: 250,
+  repeat: -1,
+  duration: 2,
+  yoyo: true,
+  rotation: 360,
+  ease: "power1.inOut",
+});
+  }, []);
 
   return (
     <main>
